14da09af8f373a84d25d4e58af6dc237415b3808
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-09-29 16:16:51 -0500

    update organization and add commnents to plot scripts

--- neumann/submit.sh
+++ neumann/submit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --ntasks=4
+#SBATCH --ntasks=5
 #SBATCH -t 0-30:00
 
 cd $SLURM_SUBMIT_DIR
 
 module load openmpi-4.1.1
 
+cp ../flash.par .
 mpirun maple container run "/home/run/flashx_neumann"
