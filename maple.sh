7353b0520eda16960e257485a912dc1eb52022c2
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-08 12:32:13 -0500

    updates for archiving

--- maple.sh
+++ maple.sh
@@ -1,8 +1,8 @@
 # chdir into directory containing the file
-cd $JOB_FILE_HOME
+cd $JOB_TREEDIR
 
-cd $JOB_FILE_HOME/neumann && rm -f Maplefile && maple config $JOB_FILE_HOME/Maplefile.simulation
-cd $JOB_FILE_HOME/outflow/buffer01 && rm -f Maplefile && maple config $JOB_FILE_HOME/Maplefile.simulation
-cd $JOB_FILE_HOME/outflow/buffer10 && rm -f Maplefile && maple config $JOB_FILE_HOME/Maplefile.simulation
-cd $JOB_FILE_HOME/outflow/buffer15 && rm -f Maplefile && maple config $JOB_FILE_HOME/Maplefile.simulation
-cd $JOB_FILE_HOME/outflow/buffer20 && rm -f Maplefile && maple config $JOB_FILE_HOME/Maplefile.simulation
+cd $JOB_TREEDIR/neumann && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
+cd $JOB_TREEDIR/outflow/buffer01 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
+cd $JOB_TREEDIR/outflow/buffer10 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
+cd $JOB_TREEDIR/outflow/buffer15 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
+cd $JOB_TREEDIR/outflow/buffer20 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
