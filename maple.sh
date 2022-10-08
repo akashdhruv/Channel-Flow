df016f9896ce15fe69c2cd60e897995254b47a02
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-08 17:03:18 -0500

    update JobTreeDir variable

--- maple.sh
+++ maple.sh
@@ -1,8 +1,8 @@
 # chdir into directory containing the file
-cd $JOB_TREEDIR
+cd $JobTreeDir
 
-cd $JOB_TREEDIR/neumann && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
-cd $JOB_TREEDIR/outflow/buffer01 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
-cd $JOB_TREEDIR/outflow/buffer10 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
-cd $JOB_TREEDIR/outflow/buffer15 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
-cd $JOB_TREEDIR/outflow/buffer20 && rm -f Maplefile && maple config $JOB_TREEDIR/Maplefile.simulation
+cd $JobTreeDir/neumann && rm -f Maplefile && maple config $JobTreeDir/Maplefile.simulation
+cd $JobTreeDir/outflow/buffer01 && rm -f Maplefile && maple config $JobTreeDir/Maplefile.simulation
+cd $JobTreeDir/outflow/buffer10 && rm -f Maplefile && maple config $JobTreeDir/Maplefile.simulation
+cd $JobTreeDir/outflow/buffer15 && rm -f Maplefile && maple config $JobTreeDir/Maplefile.simulation
+cd $JobTreeDir/outflow/buffer20 && rm -f Maplefile && maple config $JobTreeDir/Maplefile.simulation
